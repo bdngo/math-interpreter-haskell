fad70112f5b4fb4dc03c1d4711c5a0498792a3c2
Bryan Ngo <bryanngo@berkeley.edu>
2021-06-10 10:42:55 -0700

    finish RPN interpreter

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,29 +1,26 @@
-{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
-
 module Interpreter (
     interpret
 ) where
 
 import Data.Function ( on )
 import Parser ( shuntingYard )
-import Lexer (
-        Token (
-            Plus,
-            Minus,
-            Multiply,
-            Divide,
-            Modulo,
-            Power,
-            LParen,
-            RParen,
-            Number
-        ),
-        Operator,
-        isOperator
-    )
+import Lexer ( Token (..) )
 
-interpret :: (Operator a) => [a] -> Double
-interpret [] = error "empty postfix"
-interpret [Number n] = n
-interpret [_] = error "bad postfix"
-interpret (t1:t2:xs)
+interpret :: [Token] -> Token
+interpret = head . foldl foldFunc []
+
+foldFunc :: [Token] -> Token -> [Token]
+foldFunc [] t = [t]
+foldFunc [x] t = t:[x]
+foldFunc (Number n1:Number n2:ts) t =
+    case t of
+        Number n -> Number n:Number n1:Number n2:ts
+        Plus     -> Number (n1 + n2):ts
+        Minus    -> Number (n2 - n1):ts
+        Multiply -> Number (n1 * n2):ts
+        Divide   -> Number (n2 / n1):ts
+        Modulo   -> Number (fromIntegral ((mod `on` round) n2 n1)):ts
+        Power    -> Number (n2 ** n1):ts
+        _        -> error "bad parse"
+foldFunc (Number _:_:_) _ = error "bad parse"
+foldFunc (_:_:_) _ = error "bad parse"

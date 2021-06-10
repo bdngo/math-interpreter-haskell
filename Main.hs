902fddec4fa933e71859db5247c9e561b3753073
Bryan Ngo <bryanngo@berkeley.edu>
2021-06-10 12:05:13 -0700

    add print options

--- Main.hs
+++ Main.hs
@@ -3,32 +3,33 @@ module Main (
 ) where
 
 import Control.Exception ( try, SomeException (SomeException) )
+import Control.Monad ( when )
 import System.Environment ( getArgs )
 import System.IO ( stdout, hSetBuffering, BufferMode(NoBuffering) )
 import Lexer ( lexer )
 import Parser ( shuntingYard )
 import Interpreter ( interpret )
 
-loop :: Bool -> IO ()
-loop pf = do
+loop :: (Bool, Bool, Bool) -> IO ()
+loop (pf, pt, pa) = do
     res <- try (do
     putStr "calc > "
     txt <- getLine
     let tokens = lexer txt
-    if null tokens
-        then loop pf
-        else if pf
-            then print $ interpret tokens
-            else print $ interpret $ shuntingYard tokens
-    loop pf) :: IO (Either SomeException ())
+    when pt (print tokens)
+    when (null tokens) (loop (pf, pt, pa))
+    let toInterp = if pf then tokens else shuntingYard tokens
+    when (not pf && pa) (print toInterp)
+    print (interpret toInterp)
+    loop (pf, pt, pa)) :: IO (Either SomeException ())
     case res of
         Left e -> do
             print e
-            loop pf
-        Right _ -> loop pf
+            loop (pf, pt, pa)
+        Right _ -> loop (pf, pt, pa)
 
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering -- hack for IO
     argv <- getArgs
-    loop ("--postfix" `elem` argv)
+    loop ("--postfix" `elem` argv, "-t" `elem` argv, "-a" `elem` argv)
